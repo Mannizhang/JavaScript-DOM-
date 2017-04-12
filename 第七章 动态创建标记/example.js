232eef8f80c6928a927a4379d9b620f9d6691c48
Mari <1109878997man@gamil.com>
2017-04-12 23:22:37 +0800

    4.12

--- 第七章 动态创建标记/example.js
+++ 第七章 动态创建标记/example.js
@@ -7,7 +7,19 @@ function insertParagraph(text) {
     str +="</p>";
     document.write(str);
 }
+
 window.onload=function () {
     var testdiv = document.getElementById("testdiv");
     testdiv.innerHTML="<p>I inserted<em>this</em>content.</p>"
+}
+document.createElement(nodeName);
+document.createElement("p");
+window.onload=function () {
+    var para=document.createElement("p");
+    var info="nodeName:";
+    info+=para.nodeName;
+    info+="nodeType";
+    info+=para.nodeType;
+    alert(info);
+
 }
