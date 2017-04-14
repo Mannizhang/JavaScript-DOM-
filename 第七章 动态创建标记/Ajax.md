a48e8b894344f80e423d743dfb8bf2d45fae2b70
Mari <1109878997man@gamil.com>
2017-04-14 23:50:51 +0800

    4.14

---
+++ 第七章 动态创建标记/Ajax.md
@@ -0,0 +1,6 @@
+# Ajax
+- 用于概括异步加载页面内容技术
+
+## XMLHttpRequest对象
+## 渐进增强与Ajax
+## Hijax
--- 第七章 动态创建标记/example.js
+++ 第七章 动态创建标记/example.js
@@ -2,24 +2,38 @@
  * Created by maryz on 2017/4/9.
  */
 function insertParagraph(text) {
-    var str="<p";
-    str +=text;
-    str +="</p>";
+    var str = "<p";
+    str += text;
+    str += "</p>";
     document.write(str);
 }
 
-window.onload=function () {
+window.onload = function () {
     var testdiv = document.getElementById("testdiv");
-    testdiv.innerHTML="<p>I inserted<em>this</em>content.</p>"
+    testdiv.innerHTML = "<p>I inserted<em>this</em>content.</p>"
 }
 document.createElement(nodeName);
 document.createElement("p");
-window.onload=function () {
-    var para=document.createElement("p");
-    var info="nodeName:";
-    info+=para.nodeName;
-    info+="nodeType";
-    info+=para.nodeType;
+<!--创建一个p元素-->
+window.onload = function () {
+    var para = document.createElement("p");
+    var info = "nodeName:";
+    info += para.nodeName;
+    info += "nodeType";
+    info += para.nodeType;
     alert(info);
+}
+parent.appendChild(Child);
+var para = document.createElement("p");
+var testdiv = document.getElementById("testdiv");
+testdiv.appendChild(para);
+
+window.onload = function () {
+    var para = document.createElement("p");
+    var testiv = document.getElementById("testiv");
+    testiv.appendChild(para);
+    var txt = document.createTextNode("Hello World");
+    para.appendChild(txt);
 
 }
+
