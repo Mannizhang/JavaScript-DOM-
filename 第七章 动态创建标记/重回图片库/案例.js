90a21ae9ba046bab2bf3820bfe9730bb22807d0f
Mari <1109878997man@gamil.com>
2017-04-15 23:40:31 +0800

    4.15

--- 第七章 动态创建标记/重回图片库/案例.js
+++ 第七章 动态创建标记/重回图片库/案例.js
@@ -1,3 +1,11 @@
+var placeholder=document.createElement("img");
+placeholder.setAttribute("id","placeholder");
+placeholder.setAttribute("scr",image/placeholder.jpg);
+placeholder.setAttribute("alt","my image gallery");
+var description=document.createElement("p");
+description.setAttribute("id",description);
+var desctext=document.createTextNode("choose an image");
+
 function showPic(whichpic){
     if(!document.getElementById("placeholder")){return false;}
         var source=whichpic.getAttribute("href");
@@ -39,8 +47,4 @@ function addLoadEvent(func){
             }
     }
 addLoadEvent(prepareGallery);
-var placeholder=document.createElement("img");
-placeholder.setAttribute("id","placeholder");
-placeholder.setAttribute("scr",image/placeholder.jpg);
-placeholder.setAttribute()
 
