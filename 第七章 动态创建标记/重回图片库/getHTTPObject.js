0a3016d680323f1c3aa15c3920a20c446d4665c0
Mari <1109878997man@gamil.com>
2017-04-20 23:25:33 +0800

    4.20

--- 第七章 动态创建标记/重回图片库/getHTTPObject.js
+++ 第七章 动态创建标记/重回图片库/getHTTPObject.js
@@ -4,6 +4,14 @@
 function getHTTPObject() {
 if(typeof XMLHttpRequest=="undefined")
     XMLHttpRequest=function () {
-        
+     try{return new ActiveXObject("Msxml2.XMLHttp.6.0");}
+     catch (e){}
+     try{return new ActiveXObject("Msxml2.XMLHttp.3.0");}
+     catch (e){}
+     try {return new ActiveXObject("Msxml2.XMLHttp");}
+       catch (e){}
+     return false;
+
     }
+    return new XMLHttpRequest();
 }
