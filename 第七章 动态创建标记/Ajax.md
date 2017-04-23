be05e3397861441b5e649ba05ab31686342302a9
Mari <1109878997man@gamil.com>
2017-04-23 13:01:55 +0800

    4.23

---
+++ 第七章 动态创建标记/Ajax.md
@@ -0,0 +1,13 @@
+# Ajax
+- 异步的 JavaScript 和 XML
+- 不是新的编程语言，而是一种使用现有标准的新方法。
+- AJAX 是一种在无需重新加载整个网页的情况下，能够更新部分网页的技术。
+- **是一种用于创建快速动态网页的技术**
+- **通过在后台与服务器进行少量数据交换，AJAX 可以使网页实现异步更新。这意味着可以在不重新加载整个网页的情况下，对网页的某部分进行更新。**
+## XMLHttpRequest对象
+- Ajax技术的核心就是XMLHttpRequest对象
+- 所有现代浏览器均支持XMLHttpRequest对象(IE5和IE6使用ActiveXObject)
+- XMLHttpRequest 用于在后台与服务器交换数据。
+- Javascript通过这个对象可以自己发送请求，同时也自己处理响应。
+## 渐进增强与Ajax
+## Hijax
--- 第七章 动态创建标记/重回图片库/案例.js
+++ 第七章 动态创建标记/重回图片库/案例.js
@@ -1,71 +1,73 @@
 
-
-function showPic(whichpic){
-    if(!document.getElementById("placeholder")){return false;}
-        var source=whichpic.getAttribute("href");
-        var placeholder=document.getElementById("placeholder");
-        placeholder.setAttribute("src",source);
-        if(document.getElementById("description")){
-            var text=whichpic.getAttribute("title");
-            var description=document.getElementById("description");
-            description.firstChild.nodeValue=text;
-            }
-        return true;
-    }
-function prepareGallery(){
-    if(!document.getElementById){
-        return false;}
-        if(!document.getElementsByTagName){
-            return false;}
-            if(!document.getElementById("imagegallery")){
-                return false;}
-                var gallery=document.getElementById("imagegallery");
-                var links=gallery.getElementsByTagName("a");
-                for(var i=0;i<links.length;i++){
-                    links[i].onclick=function(){
-                      return!showPic(this);//或使用三元操作符：return showPic(this)?false:true;
-
-                        }
-                }
-        }
 //以下为 addLoadEvent函数代码，共享onload事件
-function addLoadEvent(func){
-    var oldonload=window.onload;
-    if(typeof window.onload!='function'){
-        window.onload=func;
-        }else{
-            window.onload=function(){
-                oldonload();
-                func();
-                }
-            }
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
     }
-var placeholder = document.createElement("img");
-placeholder.setAttribute("id", "placeholder");
-placeholder.setAttribute("scr", image / placeholder.jpg);
-placeholder.setAttribute("alt", "my image gallery");
-var description = document.createElement("p");
-description.setAttribute("id", description);
-var desctext = document.createTextNode("choose an image");
-description.appendChild(desctext);
-document.getElementsByTagName("body")[0].appendChild(placeholder);
-document.getElementsByClassName("body")[0].appendChild(description);
-
+}
+function insertAfter(newElement, targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement) {/*检查目标元素是不是parent的最后一个子元素，即比较parent元素的lastchild属性值与目标元素是否存在等于关系*/
+        parent.appendChild(newElement);
+        /*如果是，就用appendChild方法把新元素追加到parent元素上，这样新元素就恰好被插入到目标元素之后*/
+    } else {
+        parent.insertBefore(newElement, targetElement, nextSibling);
+    }
+}
 
 function preparePlaceholder() {
-if(!document.createElement)return false;
-    if(!document.createTextNode)return false;
-    if(!document.getElementById)return false;
-    if(!document.getElementById("imagegallery"))return false;
-    var placeholder=document.createElement("img");
-    placeholder.setAttribute("id","placeholder");
-    placeholder.setAttribute("scr","images/placeholder.img");
-    placeholder.setAttribute("alt","my image gallery");
-    var description=document.createElement("p");
-    description.setAttribute("id","description");
-    var desctext=document.createTextNode("choose a image");
+    if (!document.createElement)return false;
+    if (!document.createTextNode)return false;
+    if (!document.getElementById)return false;
+    if (!document.getElementById("imagegallery"))return false;
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id", "placeholder");
+    placeholder.setAttribute("scr", "images/placeholder.img");
+    placeholder.setAttribute("alt", "my image gallery");
+    var description = document.createElement("p");
+    description.setAttribute("id", "description");
+    var desctext = document.createTextNode("choose a image");
     description.appendChild(desctext);
-    var gallery=document.getElementById("imagegallery");
-    insertAfter(placeholder,gallery);
-    insertAfter(description,placeholder);
+    var gallery = document.getElementById("imagegallery");
+    insertAfter(placeholder, gallery);
+    insertAfter(description, placeholder);
 }
+function prepareGallery() {
+    if (!document.getElementById)return false;
+    if (!document.getElementsByTagName)return false;
+    if (!document.getElementById("imagegallery"))return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
+            return !showPic(this);//或使用三元操作符：return showPic(this)?false:true;
+
+        }
+        links[i].onkeypress=links[i].onclick();
+    }
+}
+function showPic(whichpic) {
+    if(!document.getElementById("placeholder"))return true;
+        var source=whichpic.getAttribute("href");
+        var placeholder=document.getElementById("placeholder");
+        placeholder.setAttribute("scr",source);
+        if(!document.getElementById("description"))return false;
+        if(whichpic.getAttribute("title")) {
+            var text = whichpic.getAttribute("title");
+        }else{
+            var text="";
+        }
+        var description=document.getElementById("description");
+        if(description.firstChild.nodeType==3){
+            description.firstChild.nodeValue=text;
+        }
+        return false;
+}
+addLoadEvent(prepareGallery());
+addLoadEvent(preparePlaceholder());
