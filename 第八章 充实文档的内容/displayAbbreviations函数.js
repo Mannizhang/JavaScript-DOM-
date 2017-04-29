8dd75c7c979bee7faa9b67740d209382cbe9ab38
Mari <1109878997man@gamil.com>
2017-04-29 23:49:06 +0800

    4.29

---
+++ 第八章 充实文档的内容/displayAbbreviations函数.js
@@ -0,0 +1,20 @@
+/**
+ * Created by maryz on 2017/4/29.
+ */
+function displayAbbreviations() {
+    var abbreviations = document.getElementsByTagNameNS("abbr");
+    if (abbreviations.length < 1)return false;
+    var defs = new Array();
+    for (var i = 0; i < abbreviations.length; i++) {
+        var definition = abbreviations[i].getAttribute("title");
+        var key = abbreviations[i].lastChild.nodeValue;
+        defs[key] = definition;
+    }
+}
+
+var header=document.createElement("h2");
+var header_text=document.createTextNode("Abbreviations");
+header.appendChild(header_text);
+document.getElementsByTagName("body")[0]
+document.body.appendChild(header);
+document.body.appendChild(dlist);
