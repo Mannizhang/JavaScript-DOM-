01bf4a968a0dfe0f4faa8e0cff25b092adba6836
Mari <1109878997man@gamil.com>
2017-04-30 23:48:53 +0800

    4.30

--- 第八章 充实文档的内容/displayAbbreviations函数.js
+++ 第八章 充实文档的内容/displayAbbreviations函数.js
@@ -11,19 +11,17 @@ function displayAbbreviations() {
         defs[key] = definition;
     }
 }
-var dlist=document.createElement("dl");
-for(key in defs){
-    var definition=defs[key];
-    var dtitle=document.createTextNode(key);
+var dlist = document.createElement("dl");
+for (key in defs) {
+    var definition = defs[key];
+    var dtitle = document.createTextNode(key);
     dtitle.appendChild(dtitle_text);
 
 }
 
 
-
-
-var header=document.createElement("h2");
-var header_text=document.createTextNode("Abbreviations");
+var header = document.createElement("h2");
+var header_text = document.createTextNode("Abbreviations");
 header.appendChild(header_text);
 document.getElementsByTagName("body")[0]
 document.body.appendChild(header);
