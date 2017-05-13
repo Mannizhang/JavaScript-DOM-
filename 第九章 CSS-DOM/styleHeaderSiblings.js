6581c52ee89105bd790e79d3ad911a532465c8a5
Mari <1109878997man@gamil.com>
2017-05-13 20:59:52 +0800

    5.13

---
+++ 第九章 CSS-DOM/styleHeaderSiblings.js
@@ -0,0 +1,6 @@
+/**
+ * Created by maryz on 2017/5/13.
+ */
+var headers=document.getElementsByTagName("h1");/*用getElementTagName的方法把所有h1元素找出来*/
+for(var i=0 ;i<headers.length;i++);/*遍历这个节点集合里的所有元素*/
+headers[i].nextSibling;
