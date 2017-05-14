318b5aabfdd245f5b8f6e2ae0173bb3151ef83ed
Mari <1109878997man@gamil.com>
2017-05-14 23:25:23 +0800

    5.14

---
+++ 第九章 CSS-DOM/highlightRows.js
@@ -0,0 +1,18 @@
+/**
+ * Created by maryz on 2017/5/14.
+ */
+function highlightRows() {
+    if(!document.getElementsByTagName)return false;
+    var rows=document.getElementsByTagName("tr");
+    for(var i =0;i<rows.length;i++){
+        rows[i].onmouseover=function () {
+            this.style.fontWeight="bold";
+
+        }
+        rows[i].onmouseout=function () {
+            this.style.fontWeight="normal";
+        }
+    }
+
+}
+addLoadEvent(highlightRows);
---
+++ 第九章 CSS-DOM/stripeTables.js
@@ -0,0 +1,22 @@
+/**
+ * Created by maryz on 2017/5/14.
+ */
+function  stripeTables() {
+    if(!document.getElementsByTagName)return false;
+    var table =ducument.getElementsByTagName("table");
+    var odd,rows;
+    for(var i=0;i<tables.length;i++){
+    odd=false;
+        rows=tables[i].getElementsByTagName("tr");
+        for(var j=0;j<rows.length;j++){
+            if(odd=true) {
+                rows[j].style.backgroundColor = "#ffc";
+                odd = false;
+            }else{
+                odd=true;
+            }
+        }
+    }
+}
+
+addLoadEvent(stripeTables);
