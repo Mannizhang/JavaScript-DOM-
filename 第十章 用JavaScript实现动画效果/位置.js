d528d381d7aebb5193fb9ba351325bfc54231296
Mari <1109878997man@gamil.com>
2017-05-17 23:03:38 +0800

    5.17

---
+++ 第十章 用JavaScript实现动画效果/位置.js
@@ -0,0 +1,13 @@
+/**
+ * Created by maryz on 2017/5/17.
+ */
+function positionMessage() {
+    if (!document.getElementById)return false;
+    if (!document.getElementById("message"))return false;
+    var elem = document.getElementById("message");
+    elem.style.position = "absolute";
+    elem.style.left = "50px";
+    elem.style.top = "1000px";
+}
+addLoadEvent(positionMessage);
+addLoadEvent(moveMessage);
