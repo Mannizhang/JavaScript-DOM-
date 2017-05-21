f911d9da2c7a4e65a0e1d0bc461cb14cd9b42bcf
Mari <1109878997man@gamil.com>
2017-05-21 23:39:05 +0800

    5.21

--- 第十章 用JavaScript实现动画效果/位置.js
+++ 第十章 用JavaScript实现动画效果/位置.js
@@ -1,7 +1,31 @@
-function positionMessage(){  
-    var elem = document.getElementById('message');  
-    elem.style.position = "relative";  
-    elem.style.left="20px";  
-    elem.style.top="50px";
-    movement=setTimeout("moveMessage()",5000);
+function positionMessage() {
+    var elem = document.getElementById('message');
+    elem.style.position = "absolute";
+    elem.style.left = "50px";
+    elem.style.top = "50px";
+    movement = setTimeout("moveMessage()", 1000);//一秒钟之后开始移动元素
 }
+function moveMessage() {
+    var elem = document.getElementById("message");
+    var xpos = parseInt(elem.style.left);
+    var ypos = parseInt(elem.style.top);
+    if (xpos == 200 && ypos == 100) {
+        return true;
+    }
+    if (xpos > 200) {
+        xpos--;//每次移动一个像素
+    }
+    if (xpos < 200) {
+        xpos++;
+    }
+    if (ypos > 200) {
+        ypos--;
+    }
+    if (ypos < 200) {
+        ypos++;
+    }
+    elem.style.left = xpos + "px";//再次转换为字符串
+    elem.style.top = ypos + "px";
+    movement = setTimeout("moveMessage()", 10);
+}
+positionMessage();
