9d5230934a4bb48c66aec3d1b173a0282ba08f8e
Mari <1109878997man@gamil.com>
2017-05-25 23:54:34 +0800

    5.25

--- 第十章 用JavaScript实现动画效果/位置.js
+++ 第十章 用JavaScript实现动画效果/位置.js
@@ -42,6 +42,46 @@ function addLoadEvent(func) {
     }
 
 }
+function moveElement(elementID,final_x,final_y,interval) {//第一步是声明它的各个参数
+    if (!document.getElementById)return false;
+    if (!document.getElementById(elementID))return false;
+    var elem = document.getElementById(elementID);
+    var xpos=parseInt(elem.style.left);
+    var ypos=parseInt(elem.style.top);//给定元素的left和top属性转换为数值，并把转换结果分别赋值给变量xpos和ypos
+    if(xpos==200&&ypos==100){//检查给定元素是否已经到达目的地
+        return true;
+    }
+    if(xpos==final_x&&ypos==final_y){
+        return true;
+    }
+    if(xpos<200){
+        xpos++;
+    }
+    if(xpos<final_x){
+        xpos++;
+    }
+    if(xpos>final_x){
+        xpos--;
+    }
+    if(ypos<final_y){
+        ypos++;
+    }
+    if(ypos>final_y){
+        ypos--;
+    }
+    elem.style.left=xpos+"px";
+    elem.style.top=ypos+"px";//它们负责把字符串"px"追加到变量xpos和ypos的末尾，并将其赋值给elem元素的left和top样式属性。
+    movement=setTimeout("moveMessage()",10)//在moveMessage函数里，每隔10毫秒调用一次moveMessage函数
+    "moveElement('"+elementID+"',"+final_y+","+final_x","+interval+")"
+    var repeat="moveElement"('"+elementID+"',"+final_y+","+final_x","+interval+")";
+
+
+
+
+
+
+
+}
 
 addLoadEvent(positionMessage);
 addLoadEvent(moveMessage);
